f0be0cda66e254aa1dbb445e215eaca1804785e9
Mauricio Figueredo <mauriciofigueredo46@gmail.com>
2020-11-06 10:16:25 -0300

    Mostrar datos
    Muestra datos que tienen el mismo nombre de titular

--- cruce_datos.py
+++ cruce_datos.py
@@ -1,7 +1,7 @@
 import sqlite3 as sq
 
 
-def conexion():
+def conexion_iprodha():
     try:
         con = sq.connect('hoja_ruta_202010.db')
         return con
@@ -10,14 +10,39 @@ def conexion():
         print(type(e))
 
 
+def conexion_samsa():
+    try:
+        con = sq.connect('padron_2020.db')
+        return con
+
+    except Exception as e:
+        print(type(e))
+
+
 def mostrar_datos():
-    conI = conexion()
-    cursosrI = conI.cursor()
-    cursosrI.execute('SELECT DNITIT, CHACRA_ADM, DIRECCION_POSTAL, CLAVE_DISTRIBUCION From hoja_ruta')
-    rows = cursosrI.fetchall()
-    for row in rows:
-        print(row)
-    conI.close()
+    con_i = conexion_iprodha()
+    con_s = conexion_samsa()
+
+    cursosrI = con_i.cursor()
+    cursorS = con_s.cursor()
+
+    cursosrI.execute('SELECT NOMBRE, CHACRA_ADM, DIRECCION_POSTAL, CLAVE_DISTRIBUCION From hoja_ruta')
+    cursorS.execute('SELECT APELLIDOYNOMBRE, CALLEPOSTAL, NROPOSTAL, CAMINANTE, POSICION from padron_2020')
+
+    beneficiarios_irpodha = cursosrI.fetchall()
+    usuarios_samsa = cursorS.fetchall()
+
+    for beneficiario in beneficiarios_irpodha:
+        for usuario in usuarios_samsa:
+            if beneficiario[0] == usuario[0]:
+                print("Resultado:")
+
+                print("Irpodha: ", beneficiario)
+                print("Samsa:   ", usuario)
+                x = input()
+
+    con_i.close()
+    con_s.close()
 
 
 if __name__ == "__main__":
