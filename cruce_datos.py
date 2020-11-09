43f11f6bd158359fe2f822ed4e6f44215cd0ed36
Mauricio Figueredo <mauriciofigueredo46@gmail.com>
2020-11-09 12:23:45 -0300

    Filtro terminado
    Compara y actualiza la clave de distribucion del archivo base del Irpodha

--- cruce_datos.py
+++ cruce_datos.py
@@ -23,13 +23,13 @@ def mostrar_datos():
     con_i = conexion_iprodha()
     con_s = conexion_samsa()
 
-    cursosrI = con_i.cursor()
+    cursorI = con_i.cursor()
     cursorS = con_s.cursor()
 
-    cursosrI.execute('SELECT NOMBRE, CHACRA_ADM, DIRECCION_POSTAL, CLAVE_DISTRIBUCION From hoja_ruta')
+    cursorI.execute('SELECT NOMBRE, CHACRA_ADM, DIRECCION_POSTAL, CLAVE_IPRODHA, CLAVE_DISTRIBUCION From hoja_ruta')
     cursorS.execute('SELECT APELLIDOYNOMBRE, CALLEPOSTAL, NROPOSTAL, CAMINANTE, POSICION from padron_2020')
 
-    beneficiarios_irpodha = cursosrI.fetchall()
+    beneficiarios_irpodha = cursorI.fetchall()
     usuarios_samsa = cursorS.fetchall()
 
     for beneficiario in beneficiarios_irpodha:
@@ -37,14 +37,29 @@ def mostrar_datos():
             if beneficiario[0] == usuario[0]:
                 # if usuario[1] in beneficiario[2]:
                 print("Resultado:")
-                print("Irpodha: ", beneficiario)
+                print("Irpodha: ", beneficiario[0], beneficiario[2], beneficiario[1], " cam: ", str(beneficiario[4]))
                 print("Samsa:   ", usuario)
-                x = input("Actualizar clave_distribución: s/n")
-                if x == 's':
-                    beneficiario[3] = usuario[3] + "-" + usuario[4]
-                    print(beneficiario)
-                    cursorS.execute('UPDATE hoja_ruta SET CLAVE_DISTRIBUCION=?'beneficiario[3]')
+                print(type(beneficiario[1]))
 
+                print("1 - Actualizar clave distribucion con datos de samsa")
+                print("2 - Asignar chacra adm")
+                print("3 - Saltear")
+                x = input("Ingrese una opcion: ")
+                if x == '1':
+                    clave_distri = str(usuario[3]) + "-" + str(usuario[4])
+                    #print(clave_distri)
+                if x == '2':
+                    clave_distri = str(beneficiario[1])
+                if x == '3':
+                    pass
+
+                if x == '1' or x == '2':
+                    try:
+                        cursorI.execute("""UPDATE hoja_ruta SET CLAVE_DISTRIBUCION=? where CLAVE_IPRODHA=?""", (clave_distri, beneficiario[3]))
+                        con_i.commit()
+                        print("Actualizacion con exito")
+                    except Exception as e:
+                        print(e)
 
     con_i.close()
     con_s.close()
